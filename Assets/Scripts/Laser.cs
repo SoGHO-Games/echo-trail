ffc84c7b34d42d360d05e0043f9480cbc6815986
Greg Hdmt <gregory.houdremont@gmail.com>
2025-06-01 11:59:34 +0200

    Started creating laser launched by a block

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform startPoint;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, startPoint.position);
+        RaycastHit hit;
+        if (Physics.Raycast(startPoint.position, Vector3.left, out hit))
+        {
+            lineRenderer.SetPosition(1, hit.point);
+            if (hit.transform.CompareTag("Player"))
+            {
+                Debug.Log("Laser hit the player!");
+            }
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, startPoint.position + Vector3.left * 100f);
+        }
+    }
+}
